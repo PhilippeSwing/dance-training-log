6684b4a825e7a9303bb5306ea5f06a49c1c16fa1
Phil <bourassa.phil@gmail.com>
2018-09-03 22:17:35 -0400

    Finish mobile size styling.

--- src/components/LogModal.js
+++ src/components/LogModal.js
@@ -15,20 +15,42 @@ class LogModal extends Component {
         return (
             <div className="past-logs__modal-container">
                 <div className="past-logs__modal">
-                    <p className="past-Log__modal__item">Date: {this.props.clickedLogInfo.date}</p>
-                    <p className="past-Log__modal__item">Partner(s): {this.props.clickedLogInfo.partners}</p>
-                    <p className="past-Log__modal__item">Topics: {this.props.clickedLogInfo.topics}</p>
-                    <p className="past-Log__modal__item"><a href={videoHref}>Video Reference</a></p>
-                    {/* <video className="past-Log__modal__item" src={videoHref} width="500px" controls></video> */}
-                    <p className="past-Log__modal__item">Prep notes: {this.props.clickedLogInfo.prepNotes}</p>
-                    <p className="past-Log__modal__item">Work in progress: {this.props.clickedLogInfo.workInProgress}</p>
-                    <p className="past-Log__modal__item">Ready for the dance floor: {this.props.clickedLogInfo.readyForDance}</p>
-                    <p className="past-Log__modal__item">New ideas: {this.props.clickedLogInfo.newIdeas}</p>
-                    <p className="past-Log__modal__item">What I learned from my partner(s): {this.props.clickedLogInfo.partnerLearn}</p>
-                    <p className="past-Log__modal__item">Game plan for next practice: {this.props.clickedLogInfo.gameNext}</p>
-                    <p className="past-Log__modal__item">Retro notes: {this.props.clickedLogInfo.retroNotes}</p>
+
+                    <h4 className="past-log__modal__header">Date:</h4>
+                    <p className="past-log__modal__item">{this.props.clickedLogInfo.date}</p>
+
+                    <h4 className="past-log__modal__header">Partner(s):</h4>
+                    <p className="past-log__modal__item">{this.props.clickedLogInfo.partners}</p>
+
+                    <h4 className="past-log__modal__header">Topics:</h4>
+                    <p className="past-log__modal__item">{this.props.clickedLogInfo.topics}</p>
+
+                    {/* <h4 className="past-log__modal__header"></h4> */}
+                    <p className="past-log__modal__item video-reference-link"><a href={videoHref}>Video Reference</a></p>
+                    {/* <video className="past-log__modal__item" src={videoHref} width="500px" controls></video> */}
+
+                    <h4 className="past-log__modal__header">Prep notes:</h4>
+                    <p className="past-log__modal__item">{this.props.clickedLogInfo.prepNotes}</p>
+
+                    <h4 className="past-log__modal__header">Work in progress:</h4>
+                    <p className="past-log__modal__item">{this.props.clickedLogInfo.workInProgress}</p>
+
+                    <h4 className="past-log__modal__header">Ready for the dance floor:</h4>
+                    <p className="past-log__modal__item">{this.props.clickedLogInfo.readyForDance}</p>
+
+                    <h4 className="past-log__modal__header">New ideas:</h4>
+                    <p className="past-log__modal__item">{this.props.clickedLogInfo.newIdeas}</p>
+
+                    <h4 className="past-log__modal__header">What I learned from my partner(s):</h4>
+                    <p className="past-log__modal__item">{this.props.clickedLogInfo.partnerLearn}</p>
+
+                    <h4 className="past-log__modal__header">Game plan for next practice:</h4>
+                    <p className="past-log__modal__item">{this.props.clickedLogInfo.gameNext}</p>
+
+                    <h4 className="past-log__modal__header">Retro notes:</h4>
+                    <p className="past-log__modal__item">{this.props.clickedLogInfo.retroNotes}</p>
                     {/* When the <p> tag is clicked, the value of the modalIsOpen property in the PastLogs state is swicthed to false, such that now the modal is no longer displayed. */}
-                    <p onClick={this.props.toggleModal} class="past-Log__modal__close-link" ><i class="past-Log__modal__close-link__icon far fa-times-circle"></i></p>
+                    <p onClick={this.props.toggleModal} className="past-log__modal__close-link"><i class="past-log__modal__close-link__icon fas fa-times-circle"></i></p>
                 </div>
             </div>
         )
